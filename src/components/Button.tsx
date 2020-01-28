cb5480e7bde24ce3ef2f3dcabf8611382d6d6003
Remy Villulles <rvillulles@sargent-disc.com>
2020-01-28 17:17:40 +0000

    Initializing styled component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 
 type ButtonProps = {
   label: string;
 };
 
 export default function Button(props: ButtonProps): React.ReactElement {
-  return <input type="submit" value={props.label} />;
+  const TheButton = styled.button``;
+
+  return <TheButton>{props.label}</TheButton>;
 }
